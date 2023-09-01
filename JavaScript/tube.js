d0d0533fe5038103cd0d3c5f70c22b5097e6f8e9
codermahbub20 <mkrefat5@gmail.com>
2023-09-02 01:16:56 +0600

    Blog section add

--- JavaScript/tube.js
+++ JavaScript/tube.js
@@ -32,25 +32,36 @@ const handleClickData = async(categoryId) =>{
         div.innerHTML = `
                 <div class="card  bg-base-100 shadow-xl">
                 <figure><img class ="w-80 h-52" src=${allData?.thumbnail} alt="Shoes" /></figure>
+
                 <div class="card-body">
                     <div class="flex gap-1">
                         <div>
                         <img class ="h-10 w-10 rounded-full" src=${allData?.authors[0]?.profile_picture} alt="">
-                        <p class="absolute sm:mt-96 md:-mt-32 md:ml-32 lg:-mt-28 lg:ml-28 text-white bg-slate-900 px-2 py-1 rounded-lg">${parseInt(allData?.others?.posted_date/3600)} Hours ${parseInt(allData?.others?.posted_date % 3600/60)} Minute </p>
+                        <p class="absolute sm:-mt-50 md:-mt-32 md:ml-32 lg:-mt-28 lg:ml-28 text-white bg-slate-900 px-2 py-1 rounded-lg">${parseInt(allData?.others?.posted_date/3600)} Hours ${parseInt(allData?.others?.posted_date % 3600/60)} Minute </p>
                         </div>
                         <h5 class ="text-xl font-bold ">${allData?.title}</h5>
                     </div>
                     <h2 class="card-title">
-                    <p>${allData?.authors[0]?.profile_name}</p>
+                    <p class="text-slate-500">${allData?.authors[0]?.profile_name}</p>
                     <img class="h-5 w-5 rounded-full" src="${allData?.authors[0]?.verified &&'blue.svg'}" alt="">
 
                     </h2>
-                    <p>${allData.others.views}  Views</p>
+                    <p>${allData.others.views} Views</p>
                 </div>
             </div>
         `
+        
         cardBody.appendChild(div);
     });
+
+}
+
+function clickMe(){
+    window.open("blog.html");
+}
+
+function clickBack(){
+    window.open("index.html");
 }
 
 loadData();
